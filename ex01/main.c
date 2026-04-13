ce5e68fa1aa0bfdf653ff14da5b7905bb226dd10
olivier martin <olimarti@f1br6s13.paris.42.school>
2026-04-13 17:25:45 +0200

    Add ex01
    Light up simple led, on port PB0

---
+++ ex01/main.c
@@ -0,0 +1,20 @@
+
+#ifndef F_CPU
+#define F_CPU 1000000UL
+#endif
+
+
+#include <avr/io.h>
+// #include <iom328p.h>
+ 
+int main()
+{
+    DDRB = 1 << DDB0; // Data Direction Bit
+    PORTB = 1 << PB0; // Value
+
+    while(1)
+    {
+
+    }
+    return 0;
+}
